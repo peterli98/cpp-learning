0b0675d346dab81f2e8ff1b1c4df030b6c8dda99
peterli98 <pedur@Peters-MBP.lan>
2018-01-27 12:23:04 -0500

    minor function change to floatcompare

--- floatCompare.cpp
+++ floatCompare.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <cmath>
 
-bool isAlmostEqual(double x, double y, double epsilon);
+bool isAlmostEqual(double x, double y, double absEpsilon, double relEpsilon);
 
 int main()
 {
     double d1(100 - 99.99);
     double d2(10 - 9.99);
-    double epsilon = 0.001;
+    double relEpsilon = 1e-8;
+    double absEpsilon = 1e-12;
 
-    if(d1 == d2 /*|| isAlmostEqual(d1, d2, epsilon) == true*/)
+    if(d1 == d2 || isAlmostEqual(d1, d2, absEpsilon, relEpsilon) == true)
     {
         std::cout << "d1 is equal to d2" << "\n";
     }
-    else if(d1 > d2 /* && isAlmostEqual(d1, d2, epsilon) == false*/)
+    else if(d1 > d2 && isAlmostEqual(d1, d2, absEpsilon, relEpsilon) == false)
     {
         std::cout << "d1 is bigger than d2" << "\n";
     }
-    else if(d1 < d2 /* && isAlmostEqual(d1, d2, epsilon) == false */)
+    else if(d1 < d2 && isAlmostEqual(d1, d2, absEpsilon, relEpsilon) == false)
     {
         std::cout << "d1 is smaller than d2" << "\n";
     }
@@ -26,7 +27,13 @@ int main()
     std::cout << d2 << "\n";
 }
 
-bool isAlmostEqual(double x, double y, double epsilon)
+bool isAlmostEqual(double x, double y, double absEpsilon, double relEpsilon)
 {
-    return fabs(x - y) <= ( (fabs(x) > fabs(b) ? fabs(x) : fabs(y)) * epsilon);
+    if (fabs(x - y) <= absEpsilon)
+    {
+        return true; 
+    }
+
+    //Knuth's algorithm
+    return fabs(x - y) <= ( (fabs(x) > fabs(y) ? fabs(x) : fabs(y)) * relEpsilon);
 }
