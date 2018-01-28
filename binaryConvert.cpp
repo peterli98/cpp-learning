066a0dc1b5ee554235fbcf3bc8e953accc28dd9b
peterli98 <pedurli98@gmail.com>
2018-01-27 20:24:30 -0500

    added .gitignore since DS_store file is annoying

---
+++ binaryConvert.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+std::string decimalToBinary(int decimalNum);
+
+int main()
+{
+    std::cout << decimalToBinary(80) << "\n";
+
+    return 0;
+}
+
+std::string decimalToBinary(int decimalNum)
+{
+    std::string binaryNum = "";
+    int remainder = 0;
+
+    while(decimalNum != 0)
+    {
+        remainder = decimalNum % 2;
+        decimalNum = decimalNum / 2;
+        binaryNum = binaryNum + std::to_string(remainder);
+    }
+    
+    reverse(binaryNum.begin(), binaryNum.end());
+
+    return binaryNum;
+}
